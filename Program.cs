add4aa9ba3f9e2f8cdbd7bd78b8850e9d9b2d0e7
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-22 00:47:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MilhasParaKm
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Olá usuário, digite um valor em milhas para receber o seu valor equivalente em quilômetros.");
+
+            int milhas = Int32.Parse (Console.ReadLine ());
+
+
+
+            int quilometros = 1609;
+
+            int resultadodamultiplicacao = milhas * quilometros; 
+
+
+
+            Console.WriteLine (resultadodamultiplicacao + " km");
+
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
